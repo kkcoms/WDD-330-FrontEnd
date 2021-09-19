e68e2121cf99664be977449d25d43ddcc27c5bde
kkcoms <kkcoms@byui.edu>
2021-09-18 19:29:48 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,43 +6,7 @@
     {
       label: "Week 01 Notes",
       url: "week1/notes.html"
-    },
-    {
-      label: "Week 02 Notes",
-      url: "week2/notes.html"
-    },    
-    {
-      label: "Week 02 Application",
-      url: "quiz/indexch4.html"
-    },
-    {
-      label: "Week 02 Team Exercise",
-      url: "week2/teamexercise.html"
-    },    
-    {
-      label: "Week 03",
-      url: "week3/"
-    },    
-    {
-      label: "Week 03 Notes",
-      url: "week3/notes.html"
-    },        
-    {
-      label: "Week 03 Team Exercise",
-      url: "week3/teamexercise.html"
-    },    
-    {
-      label: "Week 04 Application",
-      url: "week4/eloquentjsch6vector.html"
-    },    
-    {
-      label: "Week 04 Notes",
-      url: "week4/notes.html"
-    },        
-    {
-      label: "Week 04 Team Exercise",
-      url: "week4/teamexercise.html"
-    },    
+    },  
   ];
   
   const orderedlist = document.querySelector("ol");
